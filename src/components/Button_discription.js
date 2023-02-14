25bc5e46aebb9dca5aa73000f1d99aff583ba7e7
yashkholapure <yashkoffical11@gmail.com>
2023-02-14 12:11:01 +0530

    client merge

--- src/components/Button_discription.js
+++ src/components/Button_discription.js
@@ -2,15 +2,56 @@ import React, { useState } from 'react';
 import Draggable from 'react-draggable';
 import Left_container_radio from './Button_radio';
 import '../style/left_container.css'
+import Card_radio from './Card_radio';
+import Card_discriptive from './Card_discriptive'
 
-const LeftContainer = ({ onCardDrop }) => {
+const Button_discription = ({ onCardDrop }) => {
+
+
+
+  const [components,setComponents]=useState([]);
+
+  const handleDragOver=(e)=>{
+      e.preventDefault();
+  }
+
+  const handleDrop=(e)=>{
+      console.log("dropped")
+      console.log(e.dataTransfer.getData('id'))
+
+      const id=e.dataTransfer.getData('id')
+
+      if(id==='discriptive'){
+          setComponents([...components,<Card_discriptive/>])
+      }
+
+  }
+
+  const handleDragStart=(ev,id)=>{
+      ev.dataTransfer.setData('id',id)
+  }
+
+
     return (
-      <div className='question_type'>
-        <Draggable onStop={onCardDrop}>
-          <div className='discriptive'> Discriptive</div>
-        </Draggable>
+      <>
+      {/* // <div className='question_type'>
+      //   <Draggable onStop={onCardDrop}>
+      //     <div className='discriptive'> Discriptive</div>
+      //   </Draggable>
+      // </div> */}
+
+
+        <div className='question_type' onDragOver={(e)=>{handleDragOver(e)}} onDrop={(e)=>{handleDrop(e)}}>
+        
+                   
+                    <div className='discriptive' draggable onDragStart={(e)=>{handleDragStart(e,"discriptive")}}> Discriptive</div>
+                    {/* <button draggable onDragStart={(e)=>{handleDragStart(e,"button-b")}}> button -b</button> */}
+
+               
       </div>
+
+      </>
     );
   };
 
-export default LeftContainer;
+export default Button_discription;
--- src/components/Render_Form.js
+++ src/components/Render_Form.js
@@ -1,4 +1,190 @@
 
+// import React, { useEffect, useState } from "react";
+
+// import {
+//     Routes,
+//     Route,
+//     useSearchParams,
+//     BrowserRouter,
+//     useLocation
+// } from "react-router-dom"
+// import getFormById from './History'
+
+// import "../style/createdForm.css"
+// const Render_Form = () => {
+//     const [formData, setUserData] = useState([]);
+//     const location = useLocation()
+//     const [queryParameters] = useSearchParams()
+//     const dataId = queryParameters.get("id")
+//     const questionTemplate = {que:"", ans:""};
+
+//     const [questionAnswer, setQuestions] = useState([questionTemplate]);
+
+//     // const onChange = (e, index) => {
+//     //     const updatedFormData = questionAnswer.map((question, i) =>
+//     //         index == i ?
+//     //         Object.assign(question, {[e.target.answer]:e.target.value})
+//     //         :question
+        
+//     //     );
+//     //     setQuestions(updatedFormData);
+//     // }
+//     const onChange = (e, index) => {
+//         console.log(index)
+//         const updatedQuestions = [...questionAnswer];
+//         updatedQuestions[index][e.target.name] = e.target.value;
+//         console.log(e.target.value,"yuuuuuuu")
+//         setQuestions(updatedQuestions);
+//     };
+//     const handleSubmit = (e) => {
+//         const formData = questionAnswer.map((question) => {
+//             return {
+//               que: question.question,
+//               ans: question.answer,
+//             //   options: question.options,
+//             //   selectedOption: question.selectedOption,
+//             };
+//           });
+//           console.log(formData);
+//         console.log(questionAnswer,"why");
+//     };
+    
+   
+//     const getForm = async () => {
+
+//         try {
+
+//             const res = await fetch(`/api/form/getforms/` + dataId, {
+//                 method: "GET",
+//                 headers: { "Content-Type": "application/json" },
+//             })
+
+//             const data = await res.json()
+//             setUserData(data)
+
+//             if (!res.status === 200) {
+
+//                 const error = new Error(res.error)
+//                 throw error
+//             }
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     }
+
+//     useEffect(() => {
+//         getForm();
+//     }, [])
+
+//     return (
+//         <div className="main">
+//             <h2>{formData.title}</h2>
+
+//             <div className="sub-main">
+//                 <form onSubmit={(e) => handleSubmit(e)}>
+//                 {
+//                     (() => {
+//                         if (formData.questions !== undefined) {
+//                             return formData.questions.map(
+//                                 (question, index) => {
+//                                     if (question.questionType === 'discriptive') {
+//                                         return (
+//                                             <div className="question" key={index}>
+//                                                 <p className="questiontext" name="que"
+//                                                 value={questionAnswer.question}
+//                                                 onChange={(e) => onChange(e, index)}
+//                                                 >{question.questionText}</p>
+
+//                                                 <input className="answer" name="ans" type="text" value={questionAnswer.answer} 
+//                                                 onChange={(e) => onChange(e, index)}
+//                                                 />
+//                                             </div>
+//                                         )
+//                                     }
+//                                     if (question.questionType === 'radio') {
+
+//                                         return (
+//                                             <div className="question" key={index}>
+//                                                 <p className="questiont" >{question.questionText}</p>
+//                                                 {console.log(question.option[0])}
+//                                                 {question.option.map((choice, index) => (
+
+//                                                     <div className="options" key={index}>
+//                                                         <input type="radio" id={choice} name={question.question} value={choice.name} />
+//                                                         <label className="options-text" htmlFor={choice}>{choice.name}</label>
+//                                                     </div>
+
+//                                                 ))}
+
+//                                             </div>
+
+//                                         )
+
+//                                     }
+
+//                                     if (question.type === 'checkbox') {
+//                                         return (
+//                                             <div className="question" key={index}>
+//                                                 <p>{question.questionText}</p>
+//                                                 {question.options.map((choice, index) => (
+//                                                     <div className="options" key={index}>
+//                                                         <input type="checkbox" id={choice} name={question.question} value={choice.value} />
+//                                                         <label className="options-text" htmlFor={choice}>{choice.value}</label>
+//                                                     </div>
+
+//                                                 ))}
+//                                             </div>
+//                                         )
+//                                     }
+//                                     else {
+//                                         return <p> new question type</p>
+//                                     }
+//                                 }
+//                             )
+
+
+//                         } else {
+//                             return (
+//                                 <p>There is no such form</p>
+//                             )
+//                         }
+//                     })()
+//                 }
+
+//             </form>
+//             </div>
+//             <button type="submit" onClick={handleSubmit}>Submit</button>
+
+
+
+//         </div>
+
+//     )
+
+
+
+
+
+// }
+
+
+
+
+// export default Render_Form;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useEffect, useState } from "react";
 
 import {
@@ -10,15 +196,15 @@ import {
 } from "react-router-dom"
 import getFormById from './History'
 
-import "../style/createdForm.css"
+//import "../style/createdForm.css"
 const Render_Form = () => {
     const [formData, setUserData] = useState([]);
     const location = useLocation()
     const [queryParameters] = useSearchParams()
     const dataId = queryParameters.get("id")
-    const questionTemplate = {que:"", ans:""};
+  //  const questionTemplate = {que:"", ans:""};
 
-    const [questionAnswer, setQuestions] = useState([questionTemplate]);
+    const [questionAnswer, setQuestions] = useState([]);
 
     // const onChange = (e, index) => {
     //     const updatedFormData = questionAnswer.map((question, i) =>
@@ -29,24 +215,23 @@ const Render_Form = () => {
     //     );
     //     setQuestions(updatedFormData);
     // }
-    const onChange = (e, index) => {
-        console.log(index)
+    const handleChange = (e, index) => {
         const updatedQuestions = [...questionAnswer];
         updatedQuestions[index][e.target.name] = e.target.value;
-        console.log(e.target.value,"yuuuuuuu")
         setQuestions(updatedQuestions);
-    };
+      };
     const handleSubmit = (e) => {
-        const formData = questionAnswer.map((question) => {
-            return {
-              que: question.question,
-              ans: question.answer,
-            //   options: question.options,
-            //   selectedOption: question.selectedOption,
-            };
-          });
-          console.log(formData);
-        console.log(questionAnswer,"why");
+        // const formData = questionAnswer.map((question) => {
+        //     return {
+        //       que: question.question,
+        //       ans: question.answer,
+        //     //   options: question.options,
+        //     //   selectedOption: question.selectedOption,
+        //     };
+        //   });
+        //   console.log(formData);
+        // console.log(questionAnswer,"why");
+        console.log(questionAnswer,"okkkkkk")
     };
     
    
@@ -90,13 +275,16 @@ const Render_Form = () => {
                                     if (question.questionType === 'discriptive') {
                                         return (
                                             <div className="question" key={index}>
-                                                <p className="questiontext" name="que"
-                                                value={questionAnswer.question}
-                                                onChange={(e) => onChange(e, index)}
-                                                >{question.questionText}</p>
+                                                <p className="questiontext" 
+                                                   name="question"
+                                                  
+                                                   value={questionAnswer.question}
+                                                   onChange={(e) => handleChange(e, index)}
+                                                >
+                                                {question.questionText}</p>
 
-                                                <input className="answer" name="ans" type="text" value={questionAnswer.answer} 
-                                                onChange={(e) => onChange(e, index)}
+                                                <input className="answer" name="answer" type="text" value={questionAnswer.answer} 
+                                                onChange={(e) => handleChange(e, index)}
                                                 />
                                             </div>
                                         )
@@ -171,6 +359,7 @@ const Render_Form = () => {
 
 
 export default Render_Form;
+
 
 
 
